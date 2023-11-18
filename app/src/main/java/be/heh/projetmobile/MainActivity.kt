9221291ce3f506c702247910fb1dce246e4030d8
Maïckel khalaf <khalafmaickel@hotmail.com>
2023-11-19 00:26:35 +0100

    Login page mobile done

--- app/src/main/java/be/heh/projetmobile/MainActivity.kt
+++ app/src/main/java/be/heh/projetmobile/MainActivity.kt
@@ -19,13 +19,15 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        supportActionBar?.hide() // Hide the action bar
+
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
+                R.id.navigation_home, R.id.navigation_material, R.id.navigation_account))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
---
+++ app/src/main/java/be/heh/projetmobile/ui/account/AccountViewModel.kt
@@ -0,0 +1,10 @@
+package be.heh.projetmobile.ui.account
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class AccountViewModel : ViewModel() {
+
+
+}
